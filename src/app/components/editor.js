80c172ef71fec9e698061dfc708d3cef4b67bf29
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2024-07-02 11:11:38 +0330

    Add the ability to output html to the text editor

--- src/app/components/editor.js
+++ src/app/components/editor.js
@@ -1,16 +1,26 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
-
-
+import Quill from "quill";
 
 const Editor = () => {
   const [value, setValue] = useState("");
-  
+  const [editorHtml, setEditorHtml] = useState("");
+  const handleSubmit = () => {
+    const html = document.querySelector(".ql-editor").innerHTML;
+    setEditorHtml(html);
+    console.log(editorHtml);
+  };
+
+  console.log();
   return (
     <div className="">
-      <ReactQuill theme="snow" value={value} onChange={setValue} />
+      <ReactQuill value={value} onChange={setValue} />
+      <div className="editor-container">
+        <div className="ql-editor" />
+      </div>
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 };
